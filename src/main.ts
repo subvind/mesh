c46b36c9e66a27b627340c30e0acd3d5c9fb0c3c
istrav <travis.burandt@gmail.com>
2022-12-06 20:00:43 -0600

    procfile

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import 'source-map-support/register';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let port = process.env.PORT || 8080
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
